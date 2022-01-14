e4159dc93900a89d06a6ae52df4002181911d7f8
lvhaizhou <417703682@qq.com>
2022-01-14 14:45:29 +0800

    高级类型

--- src/example/advanced-type-1.ts
+++ src/example/advanced-type-1.ts
@@ -62,6 +62,76 @@ if(item1 instanceof CreatedByClass1) {
   console.log(item1.name);
 }
 
+// ! null/undefined
+// 打开tslint的：strictNullChecks
+const sumFunc = (x: number, y?: number) => x + (y || 0); // 这里y会发现它是一个 number | undefined 联合类型
+
+// !类型保护和类型断言
+const getLengthFunction = (value: string | null): number => {
+  // if(value === null) return 0;
+  // return value.length;
+  // 或者
+  return (value || '').length;
+};
+
+function getSplicedStr(num: number | null): string {
+  function getRes(prefix: string) {
+    // 因为有函数嵌套，所以这里需要使用 ! 进行类型断言
+    return prefix + num!.toFixed().toString();
+  }
+  num = num || 0.1;
+  return getRes('dylan-');
+}
+
+// !类型别名
+type TypeString = string;
+let string: TypeString;
+// 使用类型别名时，属性可以是自己
+type Childs<T> = {
+  current: T,
+  child?: Childs<T>, // 树状结构
+};
+
+// !字面量类型
+type Name = 'Dylan';
+// const name2: Name = 'Dylan1'; // 不能将类型“"Dylan1"”分配给类型“"Dylan"”。ts(2322)
+type Direction = 'north' | 'east' | 'south' | 'west';
+function getDirection(direction: Direction) {
+  return direction;
+}
+getDirection("east"); // 这里传入参数就会有提示，只能选这四个
+
+// !可辨识联合：
+// 1、具有普通的单例类型属性
+// 2、一个类型别名包含了哪些类型的联合
+interface Square {
+  kind: 'square';
+  size: number;
+}
+interface Rectangle {
+  kind: 'reactangle';
+  height: number;
+  width: number;
+}
+interface Circle {
+  kind: 'circle';
+  radius: number;
+}
+type Shape = Square | Rectangle | Circle;
+function assertNever(value: never): never {
+  throw new Error('Unexpected object: ' + value);
+}
+function getArea(s: Shape): number {
+  switch(s.kind) {
+    case "square": return s.size * s.size;
+    case "reactangle": return s.height * s.width;
+    case "circle": return Math.PI * s.radius ** 2;
+    default: return assertNever(s); // !完整性检查
+  }
+}
+
+
+
 
 
 
