1905a72ab45c978795defd50b5b90a18085e4da4
lvhaizhou <417703682@qq.com>
2022-01-20 15:13:41 +0800

    高级类型

--- src/example/advanced-type-2.ts
+++ src/example/advanced-type-2.ts
@@ -159,8 +159,143 @@ type RemoveReadonly<T> = {
   -readonly [P in keyof T]-?: T[P]
 };
 type InfoWithoutReadonly = RemoveReadonly<ReadonlyInfo1>; // 去掉所有的 readonly 和可选
-// 51分
 
+// !映射类型
+const stringIndex = 'a';
+const numberIndex = 1;
+const symbolIndex = Symbol();
+type Objs2 = {
+  [stringIndex]: string,
+  [numberIndex]: number,
+  [symbolIndex]: symbol
+};
+type keysType = keyof Objs2; // type keysType = "a" | 1 | typeof symbolIndex
+type ReadonlyTypes<T> = {
+  readonly [P in keyof T]: T[P]
+};
+let objs3: ReadonlyTypes<Objs2> = {
+  a: 'aa',
+  1: 11,
+  [symbolIndex]: Symbol()
+};
+// objs3.a = 'b'; // Error：无法分配到 "a" ，因为它是只读属性。
 
+// !元组和数组的映射类型：会生成新的元组和数组类型
+type MapToPromise<T> = {
+  [K in keyof T]: Promise<T[K]>
+};
+type Tuple = [number, string, boolean];
+type promiseTuple = MapToPromise<Tuple>;
+let tuple1: promiseTuple = [
+  new Promise((resolve) => resolve(1)),
+  new Promise((resolve) => resolve('a')),
+  new Promise((resolve) => resolve(false)),
+];
 
+// !unknown
+// 1、任何类型都可以赋值给 unknown 类型
+let value1: unknown;
+value1 = 'a';
+value1 = 1;
+
+// 2、如果没有类型断言或基于控制流的类型细化时，unknown 不可以赋值给其他类型，此时它只能赋值给 unknown 和 any 类型
+let value2: unknown;
+// let value3: string = value2; // Error：不能将类型“unknown”分配给类型“string”
+value1 = value2;
+
+// 3、如果没有类型断言或基于控制流的类型细化，不能在它上面进行任何操作
+let value4: unknown;
+// value4 += 1; // Error：运算符“+=”不能应用于类型“unknown”和“1”。
+
+// 4、unknown 与任何类型组成的交叉类型，最后都等于其他类型
+type type1 = string & unknown;
+type type2 = number & unknown;
+type type3 = string[] & unknown;
+
+// 5、unknown 与任何类型组成的联合类型（除了any），都等于 unknown 类型
+type type5 = unknown | string;
+type type6 = unknown | any;
+
+// 6、never 类型是 unknown 的子类型
+type type7 = never extends unknown ? true : false; // true
+
+// 7、keyof unknown 等于类型 never
+type type8 = keyof unknown;
+
+// 8、只能对 unknown 进行等或不等操作，不能进行其他操作
+(value1 === value2) ? value1 = true : value1 = false;
+(value1 !== value2) ? value1 = true : value1 = false;
+// value1 += value2; // Error：运算符“+=”不能应用于类型“unknown”和“unknown”。
+
+// 9、unknown 类型的值不能访问它的属性，也不能作为函数调用和作为类创建实例
+
+// 10、使用映射类型时，如果遍历的是 unknown 类型，则不会映射任何属性
+type Types1<T> = {
+  [P in keyof T]: number
+};
+type type11 = Types1<any>;
+type type12 = Types1<unknown>;
+
+// !条件表达式
+// T extends U ? X : Y
+type Types2<T> = T extends string ? string : number;
+let index1: Types2<'a'>; // string
+let index2: Types2<[]>; // number
+
+// !分布式条件类型：当待检测的类型是一个联合类型时，那该条件类型就是一个分布式条件类型，在实例化的时候，TS会自动的分化成联合类型
+type TypeName<T> = T extends any ? T : never;
+type Types3 = TypeName<string | number>; // type Types3 = string | number
+
+type TypeName1<T> =
+  T extends string ? string :
+  T extends number ? number :
+  T extends boolean ? boolean :
+  T extends undefined ? undefined :
+  T extends () => void ? () => void :
+  object;
+type Types4 = TypeName1<() => void>; // type Types4 = () => void
+type Types5 = TypeName1<string[]>; // type Types5 = object
+type Types6 = TypeName1<string[] | (() => void) | string>; // type Types6 = string | object | (() => void)
+
+type Diff<T, U> = T extends U ? never : T;
+type Test2 = Diff<string | number | boolean, undefined | number>; // type Test2 = string | boolean
+
+// 返回是function的类型
+type Types7<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never
+}[keyof T];
+interface Part {
+  id: number;
+  name: string;
+  undatePart(newName: string): void;
+}
+type Test1 = Types7<Part>;
+
+// !条件类型的类型推断：infer
+type Type8<T> = T extends any[] ? T[number] : T; // T[number]：数组值的访问
+type Test3 = Type8<string[]>; // 因为是数组，所以返回其对应值的类型：string
+
+// 使用infer
+type Type9<T> = T extends Array<infer U> ? U : T;
+type Test5 = Type9<string[]>; // string
+
+// !Exclude<T, U>：从前面类型中选出不在后面类型的
+type Type10 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>; // type Type10 = "c"
+
+// !Extract<T, U>：选取T中可以赋值给U的类型
+type Type11 = Extract<'a' | 'b' | 'c', 'c' | 'd'>; // type Type11 = "c"
+
+// !NonNullable<T>：从T中去掉 null 和 undefined
+type Type12 = NonNullable<string | number | null | undefined>; // type Type12 = string | number
+
+// !ReturnType<T>：获取函数类型返回值类型
+type Type13 = ReturnType<() => string>; // type Type13 = string
+
+// !InstanceType<T>：获取构造函数的实例类型（T extends 构造函数类型）
+class AClass {
+  constructor() {}
+}
+type T1 = InstanceType<typeof AClass>;
+type T2 = InstanceType<any>;
+// type T3 = InstanceType<string>; // Error：类型“string”不满足约束“abstract new (...args: any) => any”。
 
